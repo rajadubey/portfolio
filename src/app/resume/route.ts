85f5ee1f40a357da174cbd6d2edce1c626d1b29e
Raja Dubey <raja.dubey@oxyzo.in>
2025-11-30 11:15:11 +0530

    added resume file

--- src/app/resume/route.ts
+++ src/app/resume/route.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export async function GET() {
-  const filePath = path.join(process.cwd(), 'public', 'files', 'resume.pdf');
+  const filePath = path.join(process.cwd(), 'public', 'files', 'Resume - Raja Dubey.pdf');
   const fileBuffer = fs.readFileSync(filePath);
 
   return new NextResponse(fileBuffer, {
